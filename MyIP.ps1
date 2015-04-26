4db687f3e6438492de6d42a6c3911c6c4dfa17b7
JPBlanc <jean-paul_blanc@silogix-fr.com>
2015-04-26 18:13:49 +0200

    Add comment to MyIP.ps1

--- MyIP.ps1
+++ MyIP.ps1
@@ -1,3 +1,7 @@
+# Allow me to find my public IP using
+# http://ipecho.net/plain).content
+# Another site would be :
+# http://ipecho.net/plain
 Function MyIP
 {
   (Invoke-WebRequest -URI http://ipecho.net/plain).content
