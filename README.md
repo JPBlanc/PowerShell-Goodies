70d23b33f703160add1c7ffb05550fb3b3f517ad
JPBlanc <jean-paul_blanc@silogix-fr.com>
2015-04-28 07:56:46 +0200

    Add Test-xmldiffpatch.ps1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Functions I use in my Profile.PS1
 
-MyIPps1 : a kind of "WhoAmI" to find my public IP address.
-
+MyIPps1 : a kind of "WhoAmI" to find my public IP address (do not need add-ons). 
+Test-xmldiffpatch.ps1 : Allow to compare two XML files (need an extra DLL by instal or nugget)
---
+++ Test-xmldiffpatch.ps1
@@ -0,0 +1,21 @@
+﻿# Test-xmldiffpatch.ps1
+# This code need to download Microsoft tool
+# https://msdn.microsoft.com/en-us/library/aa302294
+# or Nuget
+# https://www.nuget.org/packages/XMLDiffPatch/
+
+# Load the DLL
+Add-Type -Path "C:\Program Files (x86)\XmlDiffPatch\Bin\xmldiffpatch.dll"
+
+$xmlD1=[XML](Get-Content 'd:\temp\M1.xml')
+$xmlD2=[XML](Get-Content 'd:\temp\M2.xml')
+$xmlWriter = [System.Xml.XmlWriter]::Create("d:\temp\M3.xml")
+
+$xmlDiff= New-Object Microsoft.XmlDiffPatch.XmlDiff
+$xmlDiff.IgnorePrefixes=$true
+$xmlDiff.IgnoreChildOrder=$true
+$xmlDiff.IgnoreNamespaces=$true
+
+$blIdentical = $xmldiff.Compare($xmlD1, $xmlD2, $xmlWriter);
+$blIdentical
+$xmlWriter.Close();
