6242c15252b1547719e33c6f4ebc32e6239c3162
JPBlanc <jean-paul_blanc@silogix-fr.com>
2015-05-05 08:38:34 +0200

    Add RegEx-MultiLine01.ps1

---
+++ RegEx-MultiLine01.ps1
@@ -0,0 +1,26 @@
+﻿# RegEx-MultiLine01.ps1
+
+Clear-Host
+
+$a =Get-Content 'D:\temp\M.xml' -raw 
+$reg = [regex]'(?sm)(  <child1.*?</child1>)'
+$tagMatches = $reg.Matches($a)
+
+$blRemoved = $true
+while ($blRemoved)
+{
+  $tagMatches = $reg.Matches($a)
+  $blRemoved = $false
+  foreach ($tagMatch in $tagMatches)
+  {
+    if ($tagMatch.value -notlike "*`"C`"*")
+    {
+      Write-Host $tagMatch.value
+      $a = $a.Remove($tagMatch.Index,$tagMatch.Length+4)
+      $blRemoved = $true
+      break
+    }
+  }
+}
+
+$a
